f0b85dbbab88fedd11021f87cd4e5376aa091c03
santi-osp <santioa2014@gmail.com>
2026-05-27 13:32:28 -0500

    feat(app): add root shell component

---
+++ src/app/app.config.ts
@@ -0,0 +1,20 @@
+import {
+  ApplicationConfig,
+  provideBrowserGlobalErrorListeners,
+  provideZoneChangeDetection
+} from '@angular/core';
+import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideRouter } from '@angular/router';
+
+import { routes } from './app.routes';
+import { authTokenInterceptor } from './core/interceptors/auth-token.interceptor';
+import { errorInterceptor } from './core/interceptors/error.interceptor';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideBrowserGlobalErrorListeners(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideHttpClient(withInterceptors([authTokenInterceptor, errorInterceptor]))
+  ]
+};
